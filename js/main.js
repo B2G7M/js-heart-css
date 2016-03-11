ca5c808fbb5b3eb797d1588bc9974aa19c644a44
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-11 08:51:19 -0500

    Initial commit
    var $header = $('<header>');
    var $h1 = $('<h1>');
    var $main = $('<main>');
    
    $(.masthead).remove;
    $h1.addClass('.logo');
    $h1.removeClass('.big-heading');
    $main.addClass('.wrapper');

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $header = $('<header>');
+var $h1 = $('<h1>');
+var $main = $('<main>');
+
+$(.masthead).remove;
+$h1.addClass('.logo');
+$h1.removeClass('.big-heading');
+$main.addClass('.wrapper');
+
+
+
