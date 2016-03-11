2c235beb081e52b22199f4bfdf2db296acbaeed5
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-11 09:25:16 -0500

    Commit incoming

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 var $header = $('<header>');
-var $h1 = $('<h1>');
-var $main = $('<main>');
+var $h1 = $('h1');
+var $main = $('main');
+var $pMain = $('p:first-child');
+var $h2 = $('h2');
+var $mainList = $('main ul');
+var $img = $('img');
+var $firstList = $('ul:first-child');
+var $lastList = $('ul:last-child');
+var $dl = $('dl');
+var $dt = $('dt');
 
-$(.masthead).remove;
-$h1.addClass('.logo');
-$h1.removeClass('.big-heading');
-$main.addClass('.wrapper');
+$h1.addClass('masthead');
+$h1.addClass('logo');
+$h1.removeClass('big-heading');
+$main.addClass('wrapper');
+$pMain.addClass('intro');
+$h2.addClass('slug-head');
+$mainList.addClass('slug-list');
+$img.addClass('slug-img');
+$firstList.addClass('slug-list-first');
+$lastList.addClass('slug-list-last');
+$dl.addClass('classification');
+$dt.removeClass('big-label');
 
-
-
+$lastList.show();
